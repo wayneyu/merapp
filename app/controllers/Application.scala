fa3ffd72ad18799a236beeebe42af96dddd03724
wayneyu <wingwa.yu@gmail.com>
2014-09-15 02:30:09 -0700

    Added  route and controller to /question/*q

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,8 @@
 package controllers
 
-import play.api._
+import scala.concurrent.ExecutionContext.Implicits._
+import play.api.libs.ws.WS
+import play.api.Play.current
 import play.api.mvc._
 
 object Application extends Controller {
@@ -12,5 +14,15 @@ object Application extends Controller {
   def team = Action{
     Ok(views.html.team("About Us"))
   }
+
+  def question(q: String) = Action.async{
+
+    val ubcwiki = WS.url("http://wiki.ubc.ca/Science:Math_Exam_Resources/" + q).get()
+
+    ubcwiki.map( response => Status(response.status)(response.body).as("text/html"))
+
+  }
+
+
 
 }

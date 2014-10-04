bf5e12ed431209f66278af4027892089bc465b1f
wayneyu <wingwa.yu@gmail.com>
2014-10-04 14:22:32 -0700

    Add more setup scripts for mongodb

--- app/models/MERquestion.scala
+++ app/models/MERquestion.scala
@@ -3,6 +3,52 @@ package models
 /**
  * Created by wayneyu on 10/4/14.
  */
-class MERquestion {
 
+
+package models
+
+import scala.reflect.BeanProperty
+
+import play.api.Play.current
+import play.modules.mongodb.jackson.MongoDB
+import play.api.libs.json._
+
+import net.vz.mongodb.jackson.{Id, ObjectId}
+
+import org.codehaus.jackson.annotate.JsonProperty
+
+
+class MERquestion(@ObjectId @Id val id: String,
+                  @BeanProperty @JsonProperty("course") val course: String,
+                  @BeanProperty @JsonProperty("exam") val exam: String,
+                  @BeanProperty @JsonProperty("question") val question: String) {
+  @ObjectId
+  @Id
+  def getId = id;
+
+
+}
+
+object MERquestionCompanion {
+
+  implicit val barReads = new Reads[Bar] {
+    def reads(js: JsValue): JsResult[Bar] = JsSuccess(
+      new Bar(
+        (js \ "id").as[String],
+        (js \ "name").as[String]
+      )
+    )
+  }
+
+  implicit val barWrites = new Writes[Bar] {
+    def writes(bar: Bar): JsValue = {
+      Json.obj(
+        "name" -> bar.name
+      )
+    }
+  }
+  private lazy val db = MongoDB.collection("bars", classOf[Bar], classOf[String])
+
+  def create(bar: Bar) { db.save(bar) }
+  def findAll() = { db.find().toArray }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,22 @@
 /**
  * Created by wayneyu on 10/4/14.
  */
-class Build {
+import sbt._
+import play.
+
+object ApplicationBuild extends Build {
+
+  val appName         = "MERAPP"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "org.webjars" % "webjars-play" % "2.1.0",
+    "org.webjars" % "jquery" % "1.9.1",
+    "net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.1.0"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+
+  )
 
 }

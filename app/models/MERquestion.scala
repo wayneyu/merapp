6a2ff8ddd908a7ec5de9615658689a215a7ec4c2
wayneyu <wingwa.yu@gmail.com>
2014-10-04 18:14:47 -0700

    Add Question class

--- app/models/MERquestion.scala
+++ app/models/MERquestion.scala
@@ -1,54 +1,19 @@
 package models
 
-/**
- * Created by wayneyu on 10/4/14.
- */
+case class Question(
+                 course: String,
+                 year: Int,
+                 term: String,
+                 statement: String,
+                 hints: List[String],
+                 sols: List[String])
 
 
-package models
+object JsonFormats {
+  import play.api.libs.json.Json
+  import play.api.data._
+  import play.api.data.Forms._
 
-import scala.reflect.BeanProperty
-
-import play.api.Play.current
-import play.modules.mongodb.jackson.MongoDB
-import play.api.libs.json._
-
-import net.vz.mongodb.jackson.{Id, ObjectId}
-
-import org.codehaus.jackson.annotate.JsonProperty
-
-
-class MERquestion(@ObjectId @Id val id: String,
-                  @BeanProperty @JsonProperty("course") val course: String,
-                  @BeanProperty @JsonProperty("exam") val exam: String,
-                  @BeanProperty @JsonProperty("question") val question: String) {
-  @ObjectId
-  @Id
-  def getId = id;
-
-
-}
-
-object MERquestionCompanion {
-
-  implicit val barReads = new Reads[Bar] {
-    def reads(js: JsValue): JsResult[Bar] = JsSuccess(
-      new Bar(
-        (js \ "id").as[String],
-        (js \ "name").as[String]
-      )
-    )
-  }
-
-  implicit val barWrites = new Writes[Bar] {
-    def writes(bar: Bar): JsValue = {
-      Json.obj(
-        "name" -> bar.name
-      )
-    }
-  }
-  private lazy val db = MongoDB.collection("bars", classOf[Bar], classOf[String])
-
-  def create(bar: Bar) { db.save(bar) }
-  def findAll() = { db.find().toArray }
+  // Generates Writes and Reads for Feed and User thanks to Json Macros
+  implicit val qFormat = Json.format[Question]
 }
--- build.sbt
+++ build.sbt
@@ -6,9 +6,19 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.11.1"
 
+resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
   cache,
-  ws
+  ws,
+  //play-mongo-jackson-mapper,
+  //"org.webjars" % "webjars-play" % "2.11.0",
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.11.0-SNAPSHOT"
+  //"org.reactivemongo" %% "play2-reactivemongo" % "0.10.2"
+  // WebJars pull in client-side web libraries
+  //"org.webjars" %% "webjars-play" % "2.1.0",
+  //"org.webjars" % "jquery" % "2.1.0"
+  //"net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.3.0"
 )

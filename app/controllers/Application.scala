028c2b24ef3711e52b969bf9678ef2507617030d
wayneyu <wingwa.yu@gmail.com>
2014-10-17 19:26:12 -0700

    Add multiple hints and solutions to view and. Changed json file path to relative

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.Play
 import play.api.libs.json.Json
 
 import scala.concurrent.ExecutionContext.Implicits._
@@ -20,21 +21,12 @@ object Application extends Controller {
   }
 
   def question(course: String, year: String, q: String) = Action {
-    val file = "/home/wayneyu/mer/raw_database/json_data/" + course + "/" + year + "/" + q + ".json"
-    //val file = Assets.at("/public/raw_database/json_data/" + course + "/" + year + "/", q + ".json");
+    val file = Play.application.getFile("/raw_database/json_data/" + course + "/" + year + "/" + q + ".json");
     val js = Json.parse(Source.fromFile(file).getLines().mkString)
     val stmt = (js \ "statement").as[String]
-    val hints: List[String] = (js \ "hints" ).as[List[String]]
-    val sols: List[String] = (js \ "sols").as[List[String]]
-//    val hint = hints(0) match {
-//      case Some(str) => str
-//      case None => ""
-//    }
-//    val sol = sols(0) match {
-//      case Some(str) => str
-//      case None => ""
-//    }
-    Ok(views.html.question(stmt, q, hints(0), sols(0)))
+    val hints = (js \ "hints" ).as[List[String]]
+    val sols = (js \ "sols").as[List[String]]
+    Ok(views.html.question(stmt, q, hints, sols))
   }
 
 

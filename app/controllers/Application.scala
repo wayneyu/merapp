2e836c0497dec7ec4e218264f32bce2864f19d53
wayneyu <wingwa.yu@gmail.com>
2014-11-17 21:28:18 -0800

    Add draft of finding exam names controller function

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -28,20 +28,4 @@ object Application extends Controller {
     Ok(views.html.editor())
   }
 
-//  def question(course: String, year: String, q: String) = Action {
-//    val inStream = Play.resourceAsStream("public/raw_database/json_data/" + course + "/" + year + "/" + q + ".json")
-//    inStream match {
-//      case Some(s) =>
-//        val js = Json.parse(Source.fromInputStream(s).getLines().mkString)
-//        val stmt = (js \ "statement").as[String]
-//        val hints = (js \ "hints").as[List[String]]
-//        val sols = (js \ "sols").as[List[String]]
-//        Ok(views.html.question(stmt, q, hints, sols))
-//      case None =>
-//        BadRequest("File not found")
-//    }
-//  }
-
-
-
 }

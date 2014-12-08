5c2ec00a485822db91d2db53f1a474d389bd8599
wayneyu <wingwa.yu@gmail.com>
2014-12-08 01:34:24 -0800

    Apply Will's changes to editor and question view. Add banner and footer to all views.

--- public/javascripts/question.js
+++ public/javascripts/question.js
@@ -10,34 +10,29 @@ $("div.hideable_box>div+div").fadeOut(0);
      });
 
     $("#latex_box #tags ul li:nth-child(1)").click(function() {   //this will apply to all anchor tags
-       $("#latex_box textarea").val($("#latex_box textarea").val()+'$x_1$'); 
+       //$("#latex_box textarea").val($("#latex_box textarea").val()+'$x_1$'); 
+       $("#latex_box textarea").insertAtCaret('$x_{1}$'); 
     });
 
     $("#latex_box #tags ul li:nth-child(2)").click(function() {   //this will apply to all anchor tags
-       $("#latex_box textarea").val($("#latex_box textarea").val()+'$\\int^4_3 x^2 dx$'); 
+       $("#latex_box textarea").insertAtCaret('$\\int_{a}^{b} f(x)\\,dx$'); 
     });
 
     $("#latex_box #tags ul li:nth-child(3)").click(function() {   //this will apply to all anchor tags
-       $("#latex_box textarea").val($("#latex_box textarea").val()+'$\\Omega + \\Pi = \\Lambda$'); 
+       $("#latex_box textarea").insertAtCaret('$\\alpha$'); 
     });
-    // $('#latex_box textarea').click(function()  
-    // { 
-    //    $(this).val($(this).val()+'STOP CLICKING ME!!\n'); 
-    // });
 
-    // cmds_latex_box = [
-    //     'sx_1$',
-    //     'sx_2$',
-    //     'sx_3$'
-    // ];
-        
+    $("#latex_box #tags ul li:nth-child(4)").click(function() {   //this will apply to all anchor tags
+       $("#latex_box textarea").insertAtCaret('$\\frac{d}{dx}$'); 
+    });
 
-    // for (i = 0; i < cmds_latex_box.length; i++) {
-        // $("#latex_box #tags ul li:nth-child(" + (i+1).toString() + ")").click(function() {   //this will apply to all anchor tags
-        //    $("#latex_box textarea").val($("#latex_box textarea").val() + cmds_latex_box[i]); 
-        //    console.log(cmds_latex_box[i]);
-        // });
-    // }
+    $("#latex_box #tags ul li:nth-child(5)").click(function() {   //this will apply to all anchor tags
+       $("#latex_box textarea").insertAtCaret('<font color="blue"> blue text </font>')
+    });
+
+    $("#latex_box #tags ul li:nth-child(6)").click(function() {   //this will apply to all anchor tags
+       $("#latex_box textarea").insertAtCaret('\n\\begin{equation}\n f(x) = g(x) + a \n\\end{equation} \n')
+    });
 
     $("#latex_box button").click(function() {   //this will apply to all anchor tags
        $("#latex_box p").empty(); 
@@ -47,6 +42,27 @@ $("div.hideable_box>div+div").fadeOut(0);
     });    
 });
 
-
-
-
+$.fn.insertAtCaret = function (tagName) {
+  return this.each(function(){
+    if (document.selection) {
+      //IE support
+      this.focus();
+      sel = document.selection.createRange();
+      sel.text = tagName;
+      this.focus();
+    }else if (this.selectionStart || this.selectionStart == '0') {
+      //MOZILLA/NETSCAPE support
+      startPos = this.selectionStart;
+      endPos = this.selectionEnd;
+      scrollTop = this.scrollTop;
+      this.value = this.value.substring(0, startPos) + tagName + this.value.substring(endPos,this.value.length);
+      this.focus();
+      this.selectionStart = startPos + tagName.length;
+      this.selectionEnd = startPos + tagName.length;
+      this.scrollTop = scrollTop;
+    } else {
+      this.value += tagName;
+      this.focus();
+    }
+  });
+};

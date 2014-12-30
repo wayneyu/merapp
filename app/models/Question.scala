156a07f7b4b2f95e498c92136d6ac6b7bec4af51
BernhardKonrad <bernhard.konrad@gmail.com>
2014-12-29 20:20:23 -0800

    Adds additional fields to Question model

--- app/models/Question.scala
+++ app/models/Question.scala
@@ -21,7 +21,13 @@ case class Question ( course: String,
                      question: String,
                      statement: String,
                      hints: List[String],
-                     sols: List[String]){
+                     sols: List[String],
+                     topics: List[String],
+                     solvers: List[String],
+                     rating: Int,
+                     num_votes: Int,
+                     flags: List[String],
+                     contributors: List[String]){
 
   def url: String = controllers.routes.QuestionController.question(course, term + "_" + year, question).url
 
@@ -39,7 +45,13 @@ object Question {
         (JsPath \ "question").read[String] and
         (JsPath \ "statement_html").read[String] and
         (JsPath \ "hints_html").read[List[String]] and
-        (JsPath \ "sols_html").read[List[String]]
+        (JsPath \ "sols_html").read[List[String]] and
+        (JsPath \ "topics").read[List[String]] and
+        (JsPath \ "solvers").read[List[String]] and
+        (JsPath \ "rating").read[Int] and
+        (JsPath \ "num_votes").read[Int] and
+        (JsPath \ "flags").read[List[String]] and
+        (JsPath \ "contributors").read[List[String]]
     )(Question.apply _)
 
   implicit object QuestionReader extends BSONDocumentReader[Question] {
@@ -51,12 +63,18 @@ object Question {
         doc.getAs[String]("question").get,
         doc.getAs[String]("statement_html").get,
         doc.getAs[List[String]]("hints_html").get,
-        doc.getAs[List[String]]("sols_html").get
+        doc.getAs[List[String]]("sols_html").get,
+        doc.getAs[List[String]]("topics").getOrElse(Nil),
+        doc.getAs[List[String]]("solvers").get,
+        doc.getAs[Int]("rating").get,
+        doc.getAs[Int]("num_votes").get,
+        doc.getAs[List[String]]("flags").get,
+        doc.getAs[List[String]]("contributors").get
       )
     }
   }
 
-  val empty = Question("",-1,"","No Question","",Nil,Nil)
+  val empty = Question("",-1,"","No Question","",Nil,Nil,Nil,Nil, -1, -1, Nil, Nil)
 
 //  def empty(course: String, year: String, term: String) =
 //    Question(course, year.toInt, term, "No Question","",Nil,Nil)

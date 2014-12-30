2869fa90f29e3efffc2170c43406d77bc68f1cd5
wayneyu <wingwa.yu@gmail.com>
2014-12-29 20:03:12 -0800

    Fix mongo drop collection command.

--- public/raw_database/import2mongo.sh
+++ public/raw_database/import2mongo.sh
@@ -3,7 +3,7 @@
 DBNAME=merdb
 COLLECTIONNAME=questions
 LIST="$(find json_data -name *.json)"
-mongoimport --db $DBNAME --eval "db.questions.drop()"
+mongo $DBNAME --eval "db.questions.drop()"
 for i in $LIST; do
 	echo $i
 	mongoimport --db $DBNAME --collection $COLLECTIONNAME --file $i --jsonArray

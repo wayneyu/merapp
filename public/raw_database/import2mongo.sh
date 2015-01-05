6321e3fc1fdf644db1b19cf14b713066982cf8f2
BernhardKonrad <bernhard.konrad@gmail.com>
2015-01-05 12:44:31 -0800

    Adds images, resizes via CSS. Closes #23.

--- public/raw_database/import2mongo.sh
+++ public/raw_database/import2mongo.sh
@@ -2,10 +2,12 @@
 #import *.json in raw_database to mongo
 DBNAME=merdb
 COLLECTIONNAME=questions
-LIST="$(find json_data -name *.json)"
 mongo $DBNAME --eval "db.questions.drop()"
+LIST="$(find json_data -name *.json)"
 for i in $LIST; do
 	echo $i
 	mongoimport --db $DBNAME --collection $COLLECTIONNAME --file $i --jsonArray
 done
-mongo $DBNAME --eval "db.questions.ensureIndex({statement_latex:\"text\",hints_latex:\"text\",solutions_latex:\"text\",answer_latex:\"text\"})"
+mongo $DBNAME --eval "db.questions.ensureIndex({statement_html:\"text\",hints_html:\"text\",solutions_html:\"text\",answer_html:\"text\"})"
+echo "\n\nTotal number of questions in the database:\n"
+mongo $DBNAME --eval "db.questions.count()"

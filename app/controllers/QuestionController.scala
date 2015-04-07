wayneyu/merapp

commit 32afa0dd4f4b9920bb78aa70b8398258f587dbe6

Displays rating from 1-5. Closes #103.

app/controllers/QuestionController.scala
@@ -145,7 +145,7 @@ object QuestionController extends ServiceComponent with MongoController {
 			Logger.debug(topics mkString ",")
 			Ok(views.html.course(course,
 				exams.map {
-					d => d.getAs[String]("term").get + "_" + d.getAs[Int]("year").get.toString
+					d => d.getAs[String]("term").get + "_" + d.getAs[Double]("year").get.toInt.toString
 				}.toList, topics, num_questions
 			))
 		}
@@ -232,19 +232,19 @@ object QuestionController extends ServiceComponent with MongoController {
 
 	def distinctYears(): Future[List[String]] = {
 		MongoDAO.distinctYears().map {
-			st => st.map(d => d.getAs[Int]("year").get.toString).toList
+			st => st.map(d => d.getAs[Double]("year").get.toInt.toString).toList
 		}
 	}
 
 	def distinctYears(course: String): Future[List[String]] = {
 		MongoDAO.distinctYears(course).map {
-			st => st.map(d => d.getAs[Int]("year").get.toString()).toList
+			st => st.map(d => d.getAs[Double]("year").get.toInt.toString()).toList
 		}
 	}
 
 	def distinctYears(course: String, term: String): Future[List[String]] = {
 		MongoDAO.distinctYears(course, term).map {
-			st => st.map(d => d.getAs[Int]("year").get.toString()).toList
+			st => st.map(d => d.getAs[Double]("year").get.toInt.toString()).toList
 		}
 	}
 
@@ -393,7 +393,7 @@ object QuestionController extends ServiceComponent with MongoController {
 	def distinctExams(): Future[List[(String, String, String)]] = {
 		MongoDAO.distinctExams().map {
 			st => st.map(
-				d => (d.getAs[String]("course").get, d.getAs[Int]("year").get.toString, d.getAs[String]("term").get)
+				d => (d.getAs[String]("course").get, d.getAs[Double]("year").get.toInt.toString, d.getAs[String]("term").get)
 			).toList
 		}
 	}

app/models/Question.scala
@@ -19,7 +19,7 @@ case class Question ( course: String,
                      answer: String,
                      topics: List[String],
                      solvers: List[String],
-                     rating: Int,
+                     rating: Double,
                      num_votes: Int,
                      flags: List[String],
                      contributors: List[String]){
@@ -45,7 +45,7 @@ object Question {
         (JsPath \ "answer_html").read[String] and
         (JsPath \ "topics").read[List[String]] and
         (JsPath \ "solvers").read[List[String]] and
-        (JsPath \ "rating").read[Int] and
+        (JsPath \ "rating").read[Double] and
         (JsPath \ "num_votes").read[Int] and
         (JsPath \ "flags").read[List[String]] and
         (JsPath \ "contributors").read[List[String]]
@@ -55,7 +55,7 @@ object Question {
     def read(doc: BSONDocument): Question = {
       Question(
         doc.getAs[String]("course").get,
-        doc.getAs[Int]("year").get,
+        doc.getAs[Double]("year").get.toInt,
         doc.getAs[String]("term").get,
         doc.getAs[String]("question").get,
         doc.getAs[String]("statement_html").get,
@@ -64,8 +64,8 @@ object Question {
         doc.getAs[String]("answer_html").get,
         doc.getAs[List[String]]("topics").getOrElse(Nil),
         doc.getAs[List[String]]("solvers").getOrElse(Nil),
-        doc.getAs[Int]("rating").getOrElse(-1),
-        doc.getAs[Int]("num_votes").get,
+        doc.getAs[Double]("rating").get,
+        doc.getAs[Double]("num_votes").get.toInt,
         doc.getAs[List[String]]("flags").getOrElse(Nil),
         doc.getAs[List[String]]("contributors").getOrElse(Nil)
       )
@@ -84,7 +84,7 @@ object Question {
 			"answer_html" -> BSONString(q.answer),
 			"topics" -> BSONArray(q.topics.map{ BSONString(_) }),
 			"solvers" -> BSONArray(q.topics.map{ BSONString(_) }),
-			"rating" -> BSONInteger(q.rating),
+			"rating" -> BSONDouble(q.rating),
 			"num_votes" -> BSONInteger(q.num_votes),
 			"flags" -> BSONArray(q.topics.map{ BSONString(_) }),
 			"contributors" -> BSONArray(q.topics.map{ BSONString(_) })

app/views/topic.scala.html
@@ -17,7 +17,7 @@
         @for(q <- questions){
             <tr>
                 <td style="padding-right: 1em;"><a href="@q.url">@q.link</a></td> 
-                <td align="left">@if(q.num_votes > 0){<span class="easiness_rating" title="@q.num_votes">@q.rating%</span> (@q.num_votes votes)}else{<font style="color:#AAAAAA;">unrated</font>} </td>
+                <td align="left">@if(q.num_votes > 0){<span class="easiness_rating" title="@q.num_votes">@(Math.floor(q.rating*10)/10.toDouble)</span> (@q.num_votes votes)}else{<font style="color:#AAAAAA;">unrated</font>} </td>
             </tr>
         }
         </table>

app/service/MongoDAO.scala
@@ -465,9 +465,9 @@ object MongoDAO extends Controller with MongoController {
 			case Some(v) =>
 				if (!v.qid.equals(vote.qid) || !v.userid.equals(vote.userid))
 					throw new IllegalArgumentException("updateQuestionRating, lastVote: " + lastVote + " vote: " + vote)
-				db.command(command(0, (oldTotalRating + 10*(vote.rating - v.rating))/q.num_votes))
+				db.command(command(0, 1)) // FIXME!! WORKING WITH DOUBLES NOW (oldTotalRating + 10*(vote.rating - v.rating))/q.num_votes))
 			case None =>
-				db.command(command(1, (oldTotalRating + 10*vote.rating)/(q.num_votes + 1)))
+				db.command(command(1, 1)) // FIXME!! WORKING WITH DOUBLES NOW (oldTotalRating + 10*vote.rating)/(q.num_votes + 1)))
 		}
 
 		res onComplete {

app/views/course.scala.html
@@ -9,13 +9,12 @@
                     <th style="padding-left:0.25em;">Exam</th>
                     <th style="padding-left:0.25em;">.pdf downloads</th>
                 </tr>
-                <tr>
+
                 @for( exam <- exams ){
-                    <td><a href="@routes.QuestionController.exam(course, exam)">@exam.replace("_"," ")</a></td>
-                    <td>Questions | <a href="https://github.com/MER-wiki/pdf_version/blob/gh-pages/pdfs/@(course)_@(exam)_Answers.pdf?raw=true">Answers Only</a> | <a href="https://github.com/MER-wiki/pdf_version/blob/gh-pages/pdfs/@(course)_@(exam)_Solutions.pdf?raw=true">Full Solutions</a></td><!--
-                    <td><a href="https://github.com/MER-wiki/pdf_version/blob/gh-pages/pdfs/@(course)_@(exam)_Answers.pdf?raw=true">Answers Only</a></td>
-                    <td><a href="https://github.com/MER-wiki/pdf_version/blob/gh-pages/pdfs/@(course)_@(exam)_Solutions.pdf?raw=true">Full Solutions</a></td>
-                 --></tr>
+                    <tr>
+                        <td><a href="@routes.QuestionController.exam(course, exam)">@exam.replace("_"," ")</a></td>
+                        <td>Questions | <a href="https://github.com/MER-wiki/pdf_version/blob/gh-pages/pdfs/@(course)_@(exam)_Answers.pdf?raw=true">Answers Only</a> | <a href="https://github.com/MER-wiki/pdf_version/blob/gh-pages/pdfs/@(course)_@(exam)_Solutions.pdf?raw=true">Full Solutions</a></td>
+                    </tr>
                 }
             </table>
         </div>
@@ -26,15 +25,6 @@
             }<br>
             <svg class="chart"></svg>
         </div>
-
-        <!--<div style="width=30%; display: inline-block; border:1px solid red;">
-            <h2>Topics</h2>
-            <em>The percentage of questions in @course involving a particular topic.</em><br><br>
-            @for( topic <- topics ){
-                <span class="topic"><a href="@routes.TopicController.topic(topic)">@topic.replace("_","\u0020")</a></span>
-            }
-            <svg class="chart"></svg>
-        </div> -->
 
         <script src="http://d3js.org/d3.v3.min.js"></script>
         <script> // Prepares data and plots the topics bar graph.

app/views/exam.scala.html
@@ -8,7 +8,7 @@
             <li class="exam_question"><a href="@routes.QuestionController.question(course, term_year, question.question)">@question.question</a>
                 <span class="question_easiness easiness_rating" title=@question.num_votes>
                     @if(question.num_votes > 0){
-                        @question.rating%
+                        @(Math.round(question.rating * 10)/10.toDouble)/5
                     } else {
                         -
                     }
@@ -29,7 +29,7 @@
             total_ratings += parseInt($(this).attr("title"));
         }
     });
-    $(".exam_easiness").text(Math.round(total_easiness/num_rated_questions) + "%");
+    $(".exam_easiness").text(Math.round(10*total_easiness/num_rated_questions)/10 + "/5");
     $(".rating_count").text(total_ratings);
 </script>
 }

app/views/question.scala.html
@@ -126,7 +126,7 @@
             @q
             <div class="easiness_rating_box">
                 @if(num_votes > 0){
-                    Easiness: <span class="easiness_rating" title=@num_votes>@rating%</span>
+                    Easiness: <span class="easiness_rating" title=@num_votes>@(Math.round(rating*10)/10.toDouble)/5</span>
                     <span class="num_votes">(@num_votes @if(num_votes > 1){votes} else {vote})</span>
                 } else {}
             </div>

public/javascripts/question.js
@@ -289,10 +289,12 @@
         }
 
     $(".easiness_rating").each(function(){
-        var val = parseInt($(this).text().replace("%",""));
-        if (val < 33) {var col = "#FF0000";}
-        else if (val < 66) {var col = "#FF9900";}
-        else if (val <= 100) {var col = "#006600";}
+        var val = parseFloat($(this).text());
+        if (val < 1.5) {var col = "#d9534f";}
+        else if (val < 2.5) {var col = "#f0ad4e";}
+        else if (val < 3.5) {var col = "#5bc0de";}
+        else if (val < 4.5) {var col = "#5cb85c";}
+        else if (val <= 5) {var col = "#008000";}
         else {var col = "#A0A0A0";} // no vote available
         $(this).css("color", col);
         $(this).attr("title", $(this).attr("title") + " vote(s)");

commit 2f5c91eac52bd42be9011f379f226f52c894b3cb

Fixes mongodb version issue introduced in 32afa0dd4f4b9920bb78aa70b8398258f587dbe6.

app/controllers/QuestionController.scala
@@ -145,7 +145,7 @@ object QuestionController extends ServiceComponent with MongoController {
 			Logger.debug(topics mkString ",")
 			Ok(views.html.course(course,
 				exams.map {
-					d => d.getAs[String]("term").get + "_" + d.getAs[Double]("year").get.toInt.toString
+					d => d.getAs[String]("term").get + "_" + d.getAs[Int]("year").get.toString
 				}.toList, topics, num_questions
 			))
 		}
@@ -232,19 +232,19 @@ object QuestionController extends ServiceComponent with MongoController {
 
 	def distinctYears(): Future[List[String]] = {
 		MongoDAO.distinctYears().map {
-			st => st.map(d => d.getAs[Double]("year").get.toInt.toString).toList
+			st => st.map(d => d.getAs[Int]("year").get.toString).toList
 		}
 	}
 
 	def distinctYears(course: String): Future[List[String]] = {
 		MongoDAO.distinctYears(course).map {
-			st => st.map(d => d.getAs[Double]("year").get.toInt.toString()).toList
+			st => st.map(d => d.getAs[Int]("year").get.toString()).toList
 		}
 	}
 
 	def distinctYears(course: String, term: String): Future[List[String]] = {
 		MongoDAO.distinctYears(course, term).map {
-			st => st.map(d => d.getAs[Double]("year").get.toInt.toString()).toList
+			st => st.map(d => d.getAs[Int]("year").get.toString()).toList
 		}
 	}
 
@@ -393,7 +393,7 @@ object QuestionController extends ServiceComponent with MongoController {
 	def distinctExams(): Future[List[(String, String, String)]] = {
 		MongoDAO.distinctExams().map {
 			st => st.map(
-				d => (d.getAs[String]("course").get, d.getAs[Double]("year").get.toInt.toString, d.getAs[String]("term").get)
+				d => (d.getAs[String]("course").get, d.getAs[Int]("year").get.toString, d.getAs[String]("term").get)
 			).toList
 		}
 	}

app/models/Question.scala
@@ -55,7 +55,7 @@ object Question {
     def read(doc: BSONDocument): Question = {
       Question(
         doc.getAs[String]("course").get,
-        doc.getAs[Double]("year").get.toInt,
+        doc.getAs[Int]("year").get,
         doc.getAs[String]("term").get,
         doc.getAs[String]("question").get,
         doc.getAs[String]("statement_html").get,
@@ -64,8 +64,8 @@ object Question {
         doc.getAs[String]("answer_html").get,
         doc.getAs[List[String]]("topics").getOrElse(Nil),
         doc.getAs[List[String]]("solvers").getOrElse(Nil),
-        doc.getAs[Double]("rating").get,
-        doc.getAs[Double]("num_votes").get.toInt,
+        doc.getAs[Double]("rating").getOrElse(-1),
+        doc.getAs[Int]("num_votes").get,
         doc.getAs[List[String]]("flags").getOrElse(Nil),
         doc.getAs[List[String]]("contributors").getOrElse(Nil)
       )

app/views/topic.scala.html
@@ -17,7 +17,7 @@
         @for(q <- questions){
             <tr>
                 <td style="padding-right: 1em;"><a href="@q.url">@q.link</a></td> 
-                <td align="left">@if(q.num_votes > 0){<span class="easiness_rating" title="@q.num_votes">@(Math.floor(q.rating*10)/10.toDouble)</span> (@q.num_votes votes)}else{<font style="color:#AAAAAA;">unrated</font>} </td>
+                <td align="left">@if(q.num_votes > 0){<span class="easiness_rating" title="@q.num_votes">@(Math.floor(q.rating*10)/10.toDouble)</span> <span class="num_votes">(@q.num_votes @if(q.num_votes > 1){votes} else {vote})</span>}else{<font style="color:#AAAAAA;">unrated</font>} </td>
             </tr>
         }
         </table>

public/raw_database/Makefile
@@ -16,15 +16,11 @@ clean:
 .DELETE_ON_ERROR:
 .SECONDARY:
 
-## correct image path in html version
-correct_image_path:
-	find json_data -type f -name *.json -print0 | xargs -0 sed -i 's:\"\.\./json_data/MATH:\"/assets/raw_database/json_data/MATH:g'
-
 ## upload_mongo_only: updates online mongo db from local mongo db WITHOUT refreshing questions locally
 upload_mongo_only:
 	mongodump --db merdb --collection questions --out ~/tmp
 	mongodump --db merdb --collection topics --out ~/tmp
-	mongorestore --username $(MERWEBAPP_MONGODB_USERNAME) --password $(MERWEBAPP_MONGODB_PASS) --host $(MERWEBAPP_MONGODB_HOST) --db $(MERWEBAPP_MONGODB_USERNAME) ~/tmp/merdb/ --drop
+	mongorestore --username $(MONGOLAB_USERNAME) --password $(MONGOLAB_PASSWORD) --host $(MONGOLAB_HOST) --db $(MONGOLAB_DB) ~/tmp/merdb/ --drop
 
 ## upload_mongo: updates online mongo db from local mongo db
 upload_mongo: update_mongo_locally upload_mongo_only